ac718eca9b095718839c2209456bfc5a9148e098
Amar Ahamed <amarahamed0615@gmail.com>
2023-07-28 11:27:43 -0400

    project org

--- POS-System/src/main/java/module-info.java
+++ POS-System/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.possystem {
 
     opens com.example.possystem to javafx.fxml;
     exports com.example.possystem;
+    exports com.example.possystem.controllers;
+    opens com.example.possystem.controllers to javafx.fxml;
 }
